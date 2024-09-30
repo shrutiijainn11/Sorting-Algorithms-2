b3901be7d0f5e31082241847f662c57505bf7d95
Shruti Jain <158989102+shrutiijainn11@users.noreply.github.com>
2024-09-30 12:18:19 +0000

    tim sort

---
+++ TimSort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class TimSortExample {
+    public static void timSort(int[] arr) {
+        Arrays.sort(arr);  // TimSort is used internally
+    }
+}
+
+// Java's built-in sort method is based on TimSort, so you don't need to implement it manually. You can use Arrays.sort().
